63bfe49326eddb4b855e1949b2fe8fc99d07e065
tellgeniewish <jadastras2@gmail.com>
2025-07-01 19:15:41 +0900

    [Feat] Region엔티티 추가

---
+++ src/main/generated/DNBN/spring/domain/QMember.java
@@ -0,0 +1,53 @@
+package DNBN.spring.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QMember is a Querydsl query type for Member
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMember extends EntityPathBase<Member> {
+
+    private static final long serialVersionUID = -1966817893L;
+
+    public static final QMember member = new QMember("member1");
+
+    public final DNBN.spring.domain.common.QBaseEntity _super = new DNBN.spring.domain.common.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath profileImage = createString("profileImage");
+
+    public final EnumPath<DNBN.spring.domain.enums.Provider> provider = createEnum("provider", DNBN.spring.domain.enums.Provider.class);
+
+    public final StringPath socialId = createString("socialId");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QMember(String variable) {
+        super(Member.class, forVariable(variable));
+    }
+
+    public QMember(Path<? extends Member> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMember(PathMetadata metadata) {
+        super(Member.class, metadata);
+    }
+
+}
+
