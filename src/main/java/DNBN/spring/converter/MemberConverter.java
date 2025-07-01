4b0b5c2526f92d5744a8404232415dc7503b2de0
tellgeniewish <jadastras2@gmail.com>
2025-07-02 02:08:48 +0900

    [Feat] MemberConverter 수정

--- src/main/java/DNBN/spring/converter/MemberConverter.java
+++ src/main/java/DNBN/spring/converter/MemberConverter.java
@@ -1,14 +1,30 @@
 package DNBN.spring.converter;
 
 import DNBN.spring.domain.Member;
+import DNBN.spring.domain.Region;
 import DNBN.spring.web.dto.MemberResponseDTO;
+import DNBN.spring.web.dto.RegionResponseDTO;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class MemberConverter {
     public static MemberResponseDTO.MemberInfoDTO toMemberInfoDTO(Member member) {
+        List<RegionResponseDTO.RegioPreviewnDTO> likePlaces = member.getLikePlaceList()
+                .stream()
+                .map(lp -> {
+                    Region region = lp.getRegion();
+                    return RegionResponseDTO.RegioPreviewnDTO.builder()
+                            .id(region.getId())
+                            .name(region.getName())
+                            .build();
+                })
+                .collect(Collectors.toList());
+
         return MemberResponseDTO.MemberInfoDTO.builder()
                 .nickname(member.getNickname())
-                .email(member.getEmail())
-                .gender(member.getGender().name())
+                .profileImage(member.getProfileImage())
+                .likePlaces(likePlaces)
                 .build();
     }
 }
