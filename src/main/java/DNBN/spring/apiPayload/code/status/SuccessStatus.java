e2038c196efa3270220a5a9a03e83114cda880a9
tellgeniewish <jadastras2@gmail.com>
2025-07-10 04:09:56 +0900

    [Feat] SuccessStatus 멤버 성공 code 수정

--- src/main/java/DNBN/spring/apiPayload/code/status/SuccessStatus.java
+++ src/main/java/DNBN/spring/apiPayload/code/status/SuccessStatus.java
@@ -14,11 +14,11 @@ public enum SuccessStatus implements BaseCode {
     
     // 멤버 관련 응답
     MEMBER_LOGIN_SUCCESS(HttpStatus.OK, "MEMBER2001", "이미 등록된 유저입니다."),
-    MEMBER_NEEDS_ONBOARDING(HttpStatus.CREATED, "MEMBER2011", "신규 유저입니다. 온보딩이 필요합니다."),
-    MEMBER_ONBOARDING_SUCCESS(HttpStatus.OK, "MEMBER2002", "온보딩 정보를 저장했습니다."),
-    MEMBER_INFO_RETRIEVED(HttpStatus.OK, "MEMBER2003", "유저 정보를 성공적으로 조회했습니다."),
-    MEMBER_LOGOUT_SUCCESS(HttpStatus.OK, "MEMBER2004", "로그아웃이 완료되었습니다."),
-    MEMBER_DELETE_SUCCESS(HttpStatus.OK, "MEMBER2005", "회원 탈퇴가 완료되었습니다.");
+    MEMBER_NEEDS_ONBOARDING(HttpStatus.CREATED, "MEMBER2002", "신규 유저입니다. 온보딩이 필요합니다."),
+    MEMBER_ONBOARDING_SUCCESS(HttpStatus.OK, "MEMBER2003", "온보딩 정보를 저장했습니다."),
+    MEMBER_INFO_RETRIEVED(HttpStatus.OK, "MEMBER2004", "유저 정보를 성공적으로 조회했습니다."),
+    MEMBER_LOGOUT_SUCCESS(HttpStatus.OK, "MEMBER2005", "로그아웃이 완료되었습니다."),
+    MEMBER_DELETE_SUCCESS(HttpStatus.OK, "MEMBER2006", "회원 탈퇴가 완료되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
