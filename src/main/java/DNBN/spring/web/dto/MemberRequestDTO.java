26dd9b6c30b147ddcc749ed77622e5da18d77e62
tellgeniewish <jadastras2@gmail.com>
2025-07-10 23:25:37 +0900

    [Test] 깃허브 pr템플릿 테스트

--- src/main/java/DNBN/spring/web/dto/MemberRequestDTO.java
+++ src/main/java/DNBN/spring/web/dto/MemberRequestDTO.java
@@ -14,7 +14,7 @@ public class MemberRequestDTO {
         @NotBlank(message = "닉네임은 필수입니다.")
         String nickname;
 
-        String profileImageUrl;
+        String profileImage;
 
         @NotEmpty(message = "좋아하는 동네는 최소 1개 이상 선택해야 합니다.")
         List<Long> chosenRegionIds; // regionId 리스트로 받음
