6bd0796bb8644c2c4fe7c27efd87f8526cc1c01a
zhwan2845 <johaeun0802@naver.com>
2025-07-12 18:10:23 +0900

    [Add] Region 필드 수정

--- src/main/java/DNBN/spring/domain/Region.java
+++ src/main/java/DNBN/spring/domain/Region.java
@@ -14,12 +14,25 @@ import java.util.List;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Region extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    @Column(length = 20, nullable = false)
+    private String province; // ex: 서울
+
+    @Column(length = 20, nullable = false)
+    private String city;     // ex: 강남구
+
+    @Column(length = 50, nullable = false)
+    private String district; // ex: 개포1동
 
     @OneToMany(mappedBy = "region", cascade = CascadeType.ALL)
     private List<LikePlace> likePlaceList = new ArrayList<>();
+
+    public String getFullName() {
+        return province + " " + city + " " + district;
+    }
 }
+
