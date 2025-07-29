aa21c7684fa43b77c7b9fa8d66c870119a528a4f
이소연 <122617471+silviasylee03@users.noreply.github.com>
2025-07-29 20:28:08 +0900

    [feat] DTO 수정

--- src/main/java/DNBN/spring/repository/CurationPlaceRepository/CurationPlaceRepository.java
+++ src/main/java/DNBN/spring/repository/CurationPlaceRepository/CurationPlaceRepository.java
@@ -1,7 +1,11 @@
 package DNBN.spring.repository.CurationPlaceRepository;
 
+import DNBN.spring.domain.Curation;
 import DNBN.spring.domain.mapping.CurationPlace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CurationPlaceRepository extends JpaRepository<CurationPlace, Long> {
+    List<CurationPlace> findByCuration(Curation curation);
 }
