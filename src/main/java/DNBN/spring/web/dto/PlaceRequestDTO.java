971168338ad2ec93c43d6fa303754cc51ba4b1ed
zhwan2845 <johaeun0802@naver.com>
2025-07-30 16:02:08 +0900

    [Refactor] PlaceRequestDTO에서 사용하지 않는 dto삭제

--- src/main/java/DNBN/spring/web/dto/PlaceRequestDTO.java
+++ src/main/java/DNBN/spring/web/dto/PlaceRequestDTO.java
@@ -14,27 +14,4 @@ public class PlaceRequestDTO {
         @Schema(description = "카테고리 ID", example = "1")
         private Long categoryId;
     }
-
-    @Getter @Setter
-    public static class MapSearchDTO {
-        @NotNull(message = "latMin은 필수입니다.")
-        @Digits(integer = 3, fraction = 5, message = "소수점 5자리까지 입력")
-        @Schema(description = "지도 화면의 최소 위도")
-        private Double latMin;
-
-        @NotNull(message = "latMax는 필수입니다.")
-        @Digits(integer = 3, fraction = 5, message = "소수점 5자리까지 입력")
-        @Schema(description = "지도 화면의 최대 위도")
-        private Double latMax;
-
-        @NotNull(message = "lngMin은 필수입니다.")
-        @Digits(integer = 3, fraction = 5, message = "소수점 5자리까지 입력")
-        @Schema(description = "지도 화면의 최소 경도")
-        private Double lngMin;
-
-        @NotNull(message = "lngMax는 필수입니다.")
-        @Digits(integer = 3, fraction = 5, message = "소수점 5자리까지 입력")
-        @Schema(description = "지도 화면의 최대 경도")
-        private Double lngMax;
-    }
 }
