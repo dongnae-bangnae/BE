c16539b4620d66867d4bdd15408d96d501a32bd2
zhwan2845 <johaeun0802@naver.com>
2025-08-19 19:53:27 +0900

    [Fix] 해당 장소 저장된 카테고리 삭제 후 장소 저장 로직 수정

--- src/main/java/DNBN/spring/service/PlaceService/PlaceCommandServiceImpl.java
+++ src/main/java/DNBN/spring/service/PlaceService/PlaceCommandServiceImpl.java
@@ -43,8 +43,8 @@ public class PlaceCommandServiceImpl implements PlaceCommandService {
                 .findByCategoryIdAndMemberAndDeletedAtIsNull(request.getCategoryId(), member)
                 .orElseThrow(() -> new CategoryHandler(ErrorStatus._FORBIDDEN));
 
-        // 4. 이미 다른 카테고리에 저장된 장소인지 검사
-        if (savePlaceRepository.existsByPlace(place)) {
+        // 4. 중복 저장 여부
+        if (savePlaceRepository.existsByPlace_PlaceIdAndCategory_Member_IdAndCategory_DeletedAtIsNull(placeId, memberId)) {
             throw new PlaceHandler(ErrorStatus.CATEGORY_ALREADY_SAVED_FOR_PLACE);
         }
 
